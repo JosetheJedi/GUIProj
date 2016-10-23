96ede33e78ab08070fb8068acfafc83dab7bb89a
Jose Hernandez <josefhu15@gmail.com>
2016-10-23 14:19:05 -0700

    Removed whitespaces

--- GUIProj/src/ImagePlayer.java
+++ GUIProj/src/ImagePlayer.java
@@ -2,16 +2,12 @@ import javax.swing.JFrame;
 
 public class ImagePlayer 
 {
-
 	public static void main(String[] args) 
 	{
 		JFrame frame = new ImageMaker();
-		
 		frame.setResizable(true);
 		frame.setTitle("Image Component Viewer");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-
 	}
-
 }
